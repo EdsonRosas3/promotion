206d93287ac3af4c79d94bed1307025c74424b32
EdsonRosas3 <edsonrosas321@gmail.com>
2021-09-09 15:29:26 -0400

    first commit

---
+++ src/test/java/com/example/promotion/service/PromotionServiceTest.java
@@ -0,0 +1,16 @@
+package com.example.promotion.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PromotionServiceTest {
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void sendEmailPromoted() {
+    }
+}
